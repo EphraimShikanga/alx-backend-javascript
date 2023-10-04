04a2c61f8e6c6d04ba1281a33c7e9895eb91748f
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-04 16:21:19 +0300

    fixed clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 function cleanSet(set, startString) {
   if (startString === '') return '';
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
-  return filteredValues.map(value => value.slice(startString.length)).join('-');
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  return filteredValues.map((value) => value.slice(startString.length)).join('-');
 }
 
 export default cleanSet;
